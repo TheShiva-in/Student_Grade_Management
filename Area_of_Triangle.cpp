d24ac422f61a8666e8804c30d55901fc1e8e62d9
shiva yadav <shivayadav11112003@gmail.com>
2026-07-21 23:10:46 +0530

    Added new basics project

---
+++ Area_of_Triangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int height=5;
+    int length=10;
+    int width=15;
+    int area;
+    area=height*length*width;
+    cout<<"Area of Triangle: "<<area;
+    return 0;
+
+
+}
